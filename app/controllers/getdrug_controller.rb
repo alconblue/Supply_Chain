675282dbc1518767459e683c708338fc60d69f6c
hks-9697-v2 <harsh.shah.hks@gmail.com>
2019-04-19 14:39:47 +0530

    demo changes

--- app/controllers/getdrug_controller.rb
+++ app/controllers/getdrug_controller.rb
@@ -1,10 +1,11 @@
 class GetdrugController < ApplicationController
   def new
   	st = "http://10.52.38.162:3000/api/org.nitk.drugtraceability.DrugBatch?access_token="+current_user.token
-    @response = RestClient::Request.execute(
+    @r = RestClient::Request.execute(
     method: :get,
     url: st,
     headers: {Accept: "application/json"}
     )
+    @response = ActiveSupport::JSON.decode(@r)
   end
 end
